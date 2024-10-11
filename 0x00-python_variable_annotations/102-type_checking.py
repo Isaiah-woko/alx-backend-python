e234aa0c77bce1beaa1c601e4960d13df39b4125
Isaiah woko <wokoisaiah68@gmail.com>
2024-10-11 16:42:56 +0100

    added task 102

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 """This module has a type-annotated function make_multiplier"""
-from typing import Tuple, Any
+from typing import Tuple, List, Any
 
 
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
-    """Returns a zoomed-in tuple by repeating each element in the tuple."""
-    zoomed_in: Tuple[Any, ...] = tuple(
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a zoomed-in list by repeating each element in the tuple."""
+    zoomed_in: List[Any] = [
         item for item in lst
-        for i in range(factor)
-    )
+        for _ in range(factor)
+    ]
     return zoomed_in
 
 
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
