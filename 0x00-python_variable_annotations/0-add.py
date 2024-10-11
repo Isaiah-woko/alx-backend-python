7bfc4d0e04814d024aa8459f347f8600a6b87024
Isaiah woko <wokoisaiah68@gmail.com>
2024-10-11 14:07:40 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+"""This module has a type-annotated function add"""
+def add(a: float, b: float) -> float:
+    """type-annotated function add that 
+        takes a float a and a float b as arguments
+        and returns their sum as a float.
+    """
+    return a + b
